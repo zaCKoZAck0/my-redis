7ee3a1a6bb742c5676bbc2999a81c405c859f94c
zackozack <id.ayushkryadav@gmail.com>
2024-09-09 06:55:45 +0530

    GET/SET

--- app/main.ts
+++ app/main.ts
@@ -7,12 +7,13 @@ console.log("Logs from your program will appear here!");
 // Uncomment this block to pass the first stage
 const server: net.Server = net.createServer((connection: net.Socket) => {
 
+    const cache = new Redis();
+
     connection.on("connect", () => {
         console.log("Connected to client");
     });
 
     connection.on("data", (data: Buffer) => {
-        const cache = new Redis();
         const commands = cache.parse(data.toString('utf-8'))
         connection.write(cache.run(commands));
     });
