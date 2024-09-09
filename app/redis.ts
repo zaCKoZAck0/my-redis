700271e93b736ba88c35bf0775429f12f8f839ea
zackozack <id.ayushkryadav@gmail.com>
2024-09-09 06:54:55 +0530

    GET/SET

--- app/redis.ts
+++ app/redis.ts
@@ -107,6 +107,7 @@ export class Redis {
                 return this.serialize("PONG");
             case COMMANDS.GET:
                 if (args.length < 2) return this.serialize(new RedisError("ERR wrong number of arguments for 'get' command"));
+                console.log(this.store);
                 return this.serialize(this.get(args[1]) as string);
             case COMMANDS.SET:
                 if (args.length < 3) return this.serialize(new RedisError("ERR wrong number of arguments for 'set' command"));
