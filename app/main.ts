b9324fe2c03265869908881ac81b28a3ea93cd18
zackozack <id.ayushkryadav@gmail.com>
2024-09-09 06:17:30 +0530

    ECHO

--- app/main.ts
+++ app/main.ts
@@ -1,19 +1,22 @@
 import * as net from "net";
+import { Redis } from "./redis";
 
 // You can use print statements as follows for debugging, they'll be visible when running tests.
 console.log("Logs from your program will appear here!");
 
 // Uncomment this block to pass the first stage
 const server: net.Server = net.createServer((connection: net.Socket) => {
-  // Handle connection
-  connection.on("data", (data: Buffer) => {
-    console.log("Received:", JSON.stringify(data.toString()));
-    for (const command of data.toString().split("\\n")) {
-        if (command.trim() === "PING") {
-            connection.write("+PONG\r\n");
-        }
-    }
-  });
+
+    connection.on("connect", () => {
+        console.log("Connected");
+    });
+
+    connection.on("data", (data: Buffer) => {
+        const cache = new Redis();
+        const commands = cache.parse(data.toString('utf-8'))
+        connection.write(cache.run(commands));
+    });
+
 });
 
 server.listen(6379, "127.0.0.1");
